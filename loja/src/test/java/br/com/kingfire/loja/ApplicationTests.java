a8968bc91b87e30c541ce8a16d1e210b8f8f04dc
otaviov <amandavv32@gmail.com>
2021-01-30 21:06:27 -0300

    Iniciando projeto Loja King fire

---
+++ loja/src/test/java/br/com/kingfire/loja/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kingfire.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
